b0112dc976921935af2b58bdea9ee150225b05a5
misikira <misikirteka@gmail.com>
2023-08-29 21:42:32 +0300

    add css modules

--- src/comp/pagenav.jsx
+++ src/comp/pagenav.jsx
@@ -1,7 +1,8 @@
 import {  NavLink} from "react-router-dom"
+import styles from"./pagenav.module.css"
 export default function Pagenav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
 
     <ul>
         <li>
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,10 +1,20 @@
-import Pagenav from "../comp/pagenav"
+import styles from "./Homepage.module.css";
+
 export default function Homepage() {
   return (
-    <div>
-        <Pagenav/>
-        <h1>worldwise spa</h1>
-       
-    </div>
-  )
+    <main className={styles.homepage}>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+      </section>
+    </main>
+  );
 }
