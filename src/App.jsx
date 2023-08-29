1f8a9fa90ec6acbce810dfdc8808c65575eb958d
misikira <misikirteka@gmail.com>
2023-08-29 20:20:13 +0300

    add page not found

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Product from './pages/Product';
 import Price from './pages/price';
 import Homepage from './pages/homepage';
+import Pagenotfound from './pages/Pagenotfound';
 export default function App() {
   return <BrowserRouter>
   <Routes>
     <Route path='/' element={<Homepage/>}/>
     <Route path="Product" element={<Product/>} />
     <Route path="Price" element={<Price/>}/>
+    <Route path="*" element={<Pagenotfound/>}/>
   </Routes>
   </BrowserRouter>
 }
