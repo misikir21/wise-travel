15053252501483af22a85917f488c7f741600361
misikira <misikirteka@gmail.com>
2023-08-30 13:37:12 +0300

    add the wise world logo

--- src/comp/pagenav.jsx
+++ src/comp/pagenav.jsx
@@ -1,21 +1,23 @@
 import {  NavLink} from "react-router-dom"
-import styles from"./pagenav.module.css"
+import Logo from "../../starter/components/Logo"
+import styles from"./PageNav.module.css"
 export default function Pagenav() {
   return (
     <nav className={styles.nav}>
-
+   <Logo/>
     <ul>
-        <li>
+    
+    <li>
         <NavLink to="/Price">Price</NavLink>
         </li>
-
-        <li>
-        <NavLink to="/Product">Product</NavLink>
-        </li>
-
         <li>
         <NavLink to="/Homepage">Home</NavLink>
         </li>
+       <li>
+        <NavLink to="/login">Login</NavLink>
+        </li>
+
+      
     </ul>
     </nav>
   )
