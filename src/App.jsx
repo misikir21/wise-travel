8a0e2dee2aaef9f628cb9abab09c4bfed2ae4120
misikira <misikirteka@gmail.com>
2023-08-30 11:14:45 +0300

    create the applayout page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Product from './pages/Product';
 import Price from './pages/price';
 import Homepage from './pages/homepage';
 import Pagenotfound from './pages/Pagenotfound';
+import AppLayout from './pages/AppLayout';
 export default function App() {
   return <BrowserRouter>
   <Routes>
     <Route path='/' element={<Homepage/>}/>
     <Route path="Product" element={<Product/>} />
     <Route path="Price" element={<Price/>}/>
+    <Route path="applications" element={<AppLayout/>}/>
     <Route path="*" element={<Pagenotfound/>}/>
   </Routes>
   </BrowserRouter>
