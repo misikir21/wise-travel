a3227856ecbea0aa6fadcddd187eb09575b0f20e
misikira <misikirteka@gmail.com>
2023-09-01 14:46:02 +0300

    fix import issues

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,8 @@
 import Sidebar from"./Sidebar"
-import styles from'./AppLayout.module.css'
 
 export default function AppLayout() {
   return (
-    <div className={styles.app}>
+    <div>s
         <Sidebar/>
     </div>
   )
--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -3,13 +3,11 @@ import styles from"./Sidebar.module.css"
 import AppNav from "./AppNav"
 import Logo from "./Logo"
 
-import AppLayout from './AppLayout'
-
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
         <Logo/>
-        <AppLayout/>
+        <AppNav/>
         <p>list of cites you visted</p>
         <footer className={styles.footer}>
             <p className={styles.copyright}>
