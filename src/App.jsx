6bf9108647baa0e5bd771256b7b640be8352f9af
misikira <misikirteka@gmail.com>
2023-09-02 19:20:16 +0300

    add useeffect to load city data

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,30 @@ import Login from './pages/Login';
 import Pagenotfound from './pages/Pagenotfound';
 import AppLayout from './pages/AppLayout';
 import CityList from './pages/CityList';
+const baseurl="http://localhost:9000"
+import { useEffect, useState } from 'react';
 export default function App() {
+  const [cites,setcites]=useState({});
+  const [isloading,setisloading]=useState(false)
+  useEffect(
+    function(){
+      async function fetchcites(){
+        try{
+          setisloading(true);
+          const res=await fetch(`${baseurl}/cities`);
+          const data=await res.json();
+          setcites(data);
+        }
+        catch{
+          alert("there is an error in loading data")
+        }
+        finally{
+          setisloading(false)
+        }
+      }
+      fetchcites();
+    },[]
+  )
   return <BrowserRouter>
   <Routes>
     <Route path='/' element={<Homepage/>}/>
