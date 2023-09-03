6ca3bc33d4289299a3782c96868227556d8ac7f3
misikira <misikirteka@gmail.com>
2023-09-03 20:41:04 +0300

    update the readme of project

--- src/main.jsx
+++ src/main.jsx
@@ -24,16 +24,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-
-
-
