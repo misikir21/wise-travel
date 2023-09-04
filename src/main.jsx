4ddce893890c22eefbccd8181eab64f2618a126e
misikira <misikirteka@gmail.com>
2023-09-04 13:34:49 +0300

    update the readme

--- src/main.jsx
+++ src/main.jsx
@@ -9,18 +9,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 )
 
-
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-
-
-
--- src/pages/CityList.jsx
+++ src/pages/CityList.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import styles from './CityList.module.css'
 import Spinner from './Spinner'
 import Cityitem from './Cityitem'
 
-export default function CityList(cites,isloading) {
+export default function CityList({cities,isloading}) {
  if (isloading) return <Spinner />
   return (
     <ul className={styles.CityList}>
-    {city.map(city=>
+    {cities.map(city=>
     <Cityitem city={city} key={city.id}/>)}
     </ul>
   )
